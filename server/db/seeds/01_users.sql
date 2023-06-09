9930068b6182630a011a726e1ac8b465e90bfca9
Viktor Ristic <vktr.r2@protonmail.com>
2023-06-08 22:04:22 -0400

    Add seed data files

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,6 +1,12 @@
-INSERT INTO users (email, password, first_name, last_name) VALUES ('rick.sandchez@gmail.com', 'picklerick', 'Ada', 'Avis');
-INSERT INTO users (email, password, first_name, last_name) VALUES ('lisa.simpson@gmail.com', 'ehhhhh', 'Ben', 'Bond');
-INSERT INTO users (email, password, first_name, last_name) VALUES ('link@yahoo.com', 'hyrule', 'Cathy', 'Capernick');
-INSERT INTO users (email, password, first_name, last_name) VALUES ('simon_bel123@mail.ca', 'dracula', 'Dennis', 'Drivel');
-INSERT INTO users (email, password, first_name, last_name) VALUES ('all_might@academia.jp', 'plusUltra', 'Edna', 'Erikson');
-INSERT INTO users (email, password, first_name, last_name) VALUES ('mario@mushroomkindom.jp', 'plumber79', 'Frank', 'Farder');
+INSERT INTO users (email, password, first_name, last_name, phone, is_employee, profile_img) 
+VALUES 
+('user1@example.com', 'password1', 'John', 'Doe', '123-456-7890', FALSE, 'img1.jpg'),
+('user2@example.com', 'password2', 'Jane', 'Doe', '234-567-8901', FALSE, 'img2.jpg'),
+('user3@example.com', 'password3', 'Jim', 'Smith', '345-678-9012', FALSE, 'img3.jpg'),
+('user4@example.com', 'password4', 'Jill', 'Smith', '456-789-0123', FALSE, 'img4.jpg'),
+('user5@example.com', 'password5', 'Jerry', 'Brown', '567-890-1234', FALSE, 'img5.jpg'),
+('user6@example.com', 'password6', 'Jessica', 'Brown', '678-901-2345', FALSE, 'img6.jpg'),
+('user7@example.com', 'password7', 'Jack', 'Black', '789-012-3456', FALSE, 'img7.jpg'),
+('user8@example.com', 'password8', 'Jenny', 'Black', '890-123-4567', FALSE, 'img8.jpg'),
+('user9@example.com', 'password9', 'Jake', 'White', '901-234-5678', FALSE, 'img9.jpg'),
+('user10@example.com', 'password10', 'Julia', 'White', '012-345-6789', FALSE, 'img10.jpg');
---
+++ server/db/seeds/03_orders.sql
@@ -0,0 +1,10 @@
+INSERT INTO orders (user_id, total_price_cents, stripe_charge_id, longitude, latitude, location_description) VALUES
+(1, 13000, 'charge_1abcd', 43.639622, -79.461613, 'High Park, Toronto'),
+(1, 90000, 'charge_2abcd', 43.865390, -78.839318, 'Oshawa Valley Lands'),
+(1, 5600, 'charge_3abcd', 43.267868, -79.906244, 'Churchill Park'),
+(2, 36700, 'charge_4abcd', 43.639622, -79.461613, 'High Park, Toronto'),
+(3, 11000, 'charge_5abcd', 43.865390, -78.839318, 'Oshawa Valley Lands'),
+(3, 4000, 'charge_6abcd', 43.267868, -79.906244, 'Churchill Park'),
+(3, 6900, 'charge_7abcd', 43.639622, -79.461613, 'High Park, Toronto'),
+(4, 10000, 'charge_8abcd', 43.865390, -78.839318, 'Oshawa Valley Lands'),
+(5, 10000, 'charge_9abcd', 43.267868, -79.906244, 'Churchill Park');
