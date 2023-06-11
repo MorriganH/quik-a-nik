2ee134e0872279e14694bcd4102f483ef33e65ad
Viktor Ristic <vktr.r2@protonmail.com>
2023-06-11 14:19:06 -0400

    Install and initialize Stripe and CardField component

--- frontend/App.js
+++ frontend/App.js
@@ -9,6 +9,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import * as Device from "expo-device";
 import Stripe from "./components/Stripe";
+import { StripeProvider } from "@stripe/stripe-react-native";
 
 export default function App() {
   let device;
@@ -56,6 +57,12 @@ export default function App() {
 
   //App return
   return (
+
+    <StripeProvider
+      publishableKey="pk_test_51NDgmwLv74N28uF2mIdIFKFUrZwHKfWUM4hWPUKUWoklWLq4RUD2YYIBkjSWftEFPdrkXzhfym0zYh2H98u7Atan00PKp1jbjr"
+      // urlScheme="your-url-scheme" // required for 3D Secure and bank redirects
+      // merchantIdentifier="merchant.com.{{YOUR_APP_NAME}}" // required for Apple Pay
+    >
     <View style={styles.container}>
       {view === "HOME" && (
         <Home
@@ -78,5 +85,6 @@ export default function App() {
       )}
       <StatusBar style="auto" />
     </View>
+    </StripeProvider>
   );
 }
--- frontend/components/Stripe.js
+++ frontend/components/Stripe.js
@@ -5,10 +5,45 @@ import * as Device from "expo-device";
 import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import key from "../api_key";
 import * as Location from "expo-location";
+import { CardField, useStripe } from "@stripe/stripe-react-native";
 
 export default function Stripe() {
-
   return (
-    <Text> Test stripe page? </Text>
-  )
+
+    
+    <View style={styles.container}>
+        <Text> Test stripe page? </Text>
+        <CardField
+          postalCodeEnabled={true}
+          placeholders={{
+            number: "4242 4242 4242 4242",
+          }}
+          cardStyle={{
+            backgroundColor: "#FFFFFF",
+            textColor: "#000000",
+          }}
+          style={{
+            width: "100%",
+            height: 50,
+            marginVertical: 30,
+          }}
+          onCardChange={(cardDetails) => {
+            console.log("cardDetails", cardDetails);
+          }}
+          onFocus={(focusedField) => {
+            console.log("focusField", focusedField);
+          }}
+        />
+      </View>
+
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+});
