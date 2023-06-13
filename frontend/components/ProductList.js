cafd6d188ad3b700cc01856591e396dc6b31246c
MorriganH <morriganhennessy@gmail.com>
2023-06-13 17:30:59 -0400

    completes most styling of product list page

--- frontend/components/ProductList.js
+++ frontend/components/ProductList.js
@@ -1,37 +1,46 @@
 import React, { useState, useEffect } from "react";
-import {
-  Text,
-  View,
-  Image,
-  Button,
-  Platform,
-  FlatList,
-} from "react-native";
-import styles from '../styles/productList'
+import { Text, View, Image, Button, Platform, FlatList } from "react-native";
+import styles from "../styles/productList";
 import { useDispatch, useSelector } from "react-redux";
 import { addItem } from "../redux/actions";
 
 export default function ProductList() {
   const device = Platform.OS;
+  const columns = device === 'web' ? 3 : 1;
 
-  const { cart, products } = useSelector((state) => state.reducer);
+  const { cart, products } = useSelector(state => state.reducer);
   const dispatch = useDispatch();
 
-  console.log(cart);
 
   const Item = ({ product }) => (
     <View style={styles.item}>
-      <Text style={styles.title}>{product.name}</Text>
-      <Button title="Add to cart" onPress={() => dispatch(addItem(product))} />
+      <Image
+        style={styles.logo}
+        source={require("../assets/Juniper_Twitter_Art.webp")}
+      />
+      <View style={styles.prodInfo}>
+        <Text style={styles.prodName}>{product.name}</Text>
+        <Text>${product.price_cents / 100}</Text>
+        <Button
+          color="#55bb55"
+          title="Add to cart"
+          onPress={() => dispatch(addItem(product))}
+        />
+      </View>
     </View>
   );
 
   return (
-    <FlatList
-      data={products}
-      renderItem={(product) => <Item product={product.item} />}
-      keyExtractor={(item) => item.id}
-    />
+    <View style={styles.list}>
+      <Text style={styles.title}>Take your pick!</Text>
+      <Text style={styles.subtitle}>{device === 'web' ? 'Click' : 'Touch'} an item to view more info</Text>
+      <FlatList
+        data={products}
+        numColumns={columns}
+        renderItem={product => <Item product={product.item} />}
+        keyExtractor={item => item.id}
+      />
+    </View>
 
     // productsArr;
   );
--- frontend/styles/productList.js
+++ frontend/styles/productList.js
@@ -1,24 +1,94 @@
-import { StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({
-  prod: {
-    // border: "solid",
-    padding: 5,
-    margin: 5,
-    width: "30%",
-  },
-  container: {
-    flex: 1,
-  },
-  item: {
-    backgroundColor: "white",
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
-});
+const device = Platform.OS;
+let styles;
+
+if (device === "web") {
+  styles = StyleSheet.create({
+    list: {
+      display: "flex",
+      alignItems: "center",
+    },
+
+    item: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-around",
+      alignItems: "center",
+      backgroundColor: "white",
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+      borderRadius: 10,
+      width: "30%",
+    },
+
+    logo: {
+      width: 100,
+      height: 100,
+    },
+
+    title: {
+      fontSize: 32,
+    },
+
+    subtitle: {
+      fontSize: 20,
+    },
+
+    prodInfo: {
+      display: "flex",
+      flexDirection: "column",
+      padding: 10,
+    },
+
+    prodName: {
+      width: 200,
+      fontSize: 24,
+    },
+  });
+}
+
+if (device !== "web") {
+  styles = StyleSheet.create({
+    // prod: {
+    //   // border: "solid",
+    //   padding: 5,
+    //   margin: 5,
+    //   width: "30%",
+    // },
+
+    // container: {
+    //   flex: 1,
+    // },
+    list: {
+      display: "flex",
+      alignItems: "center",
+    },
+
+    item: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-around",
+      alignItems: "center",
+      backgroundColor: "white",
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+      borderRadius: 10,
+      // width: '33%'
+    },
+
+    logo: {
+      width: 100,
+      height: 100,
+    },
+
+    title: {
+      width: 200,
+      fontSize: 24,
+    },
+  });
+}
 
 export default styles;
