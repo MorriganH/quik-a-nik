5567362900e5d0e5325b2b8e991c19434a308328
MorriganH <morriganhennessy@gmail.com>
2023-06-15 19:06:29 -0400

    adds styling to login page

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import { setUserSession } from "../redux/actions";
 import { useDispatch } from "react-redux";
 import bcrypt from "bcryptjs";
+import styles from "../styles/login";
 
 export default function Login({ navigation }) {
   const [email, setEmail] = useState("");
@@ -21,7 +22,7 @@ export default function Login({ navigation }) {
     const input = { email: email.toLowerCase(), password };
 
     axios
-      .post(`${tunnelURL}/users/login`, {email: input.email})
+      .post(`${tunnelURL}/users/login`, { email: input.email })
       .then(res => {
         console.log(res);
         if (!res.data) {
@@ -29,8 +30,7 @@ export default function Login({ navigation }) {
           throw Error("User credentials invalid");
         }
 
-        return bcrypt.compare(password, res.data.password)
-        .then(passed => {
+        return bcrypt.compare(password, res.data.password).then(passed => {
           if (!passed) {
             alert("Login failed. Check your email and password.");
             throw Error("User credentials invalid");
@@ -45,44 +45,32 @@ export default function Login({ navigation }) {
   };
 
   return (
-    <View>
-      <Text>Email</Text>
-      <TextInput
-        style={{
-          height: 40,
-          width: 300,
-          borderColor: "gray",
-          borderWidth: 1,
-        }}
-        placeholder="email"
-        inputMode="email"
-        onChangeText={newText => setEmail(newText)}
-        onSubmitEditing={() => userAuth(email, password)}
-      />
-      <Text>Password</Text>
-      <TextInput
-        style={{
-          height: 40,
-          width: 300,
-          borderColor: "gray",
-          borderWidth: 1,
-        }}
-        placeholder="password"
-        onChangeText={newText => setPassword(newText)}
-        onSubmitEditing={() => userAuth(email, password)}
-        secureTextEntry={true}
-      />
-      <Pressable
-        style={{
-          margin: 20,
-          height: 40,
-          width: 100,
-          backgroundColor: "red",
-        }}
-        onPress={() => userAuth(email, password)}
-      >
-        <Text>Submit</Text>
-      </Pressable>
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.title}>Login</Text>
+        <Text>Email</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="email"
+          inputMode="email"
+          onChangeText={newText => setEmail(newText)}
+          onSubmitEditing={() => userAuth(email, password)}
+        />
+        <Text>Password</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="password"
+          onChangeText={newText => setPassword(newText)}
+          onSubmitEditing={() => userAuth(email, password)}
+          secureTextEntry={true}
+        />
+        <Pressable
+          style={styles.submitButton}
+          onPress={() => userAuth(email, password)}
+        >
+          <Text>Submit</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
---
+++ frontend/styles/login.js
@@ -0,0 +1,44 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  box: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#F5FCFF",
+    width: "80%",
+    height: "50%",
+    borderRadius: 15,
+  },
+
+  title: {
+    fontSize: 34,
+    paddingBottom: 20,
+  },
+
+  textInput: {
+    height: 40,
+    width: 300,
+    fontSize: 20,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+
+  submitButton: {
+    borderWidth: 1,
+    marginTop: 10,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "#55bb55",
+  },
+});
+
+export default styles;
