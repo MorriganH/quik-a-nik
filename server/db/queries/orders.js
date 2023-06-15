ccd9c8361b852027850e9caf9200599433a42c19
Viktor Ristic <vktr.r2@protonmail.com>
2023-06-15 18:51:08 -0400

    Update Orders query to capture customer and product related data

--- server/db/queries/orders.js
+++ server/db/queries/orders.js
@@ -2,7 +2,9 @@ const db = require("../../configs/db.config");
 
 const getOrders = () => {
   return db
-    .query("SELECT * FROM orders ORDER BY created_at DESC;")
+    .query(
+      "SELECT orders.id, orders.user_id, orders.total_price_cents, orders.longitude, orders.latitude, orders.created_at, line_items.quantity, line_items.line_price_cents, products.name, users.first_name, users.email FROM orders JOIN line_items ON orders.id = line_items.order_id JOIN products ON line_items.product_id = products.id JOIN users ON users.id = orders.user_id ORDER BY orders.created_at DESC;"
+    )
     .then((data) => {
       return data.rows;
     });
