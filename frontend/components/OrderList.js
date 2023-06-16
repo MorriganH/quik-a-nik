012d80752a09955077d4930225168289f3ea6544
Viktor Ristic <vktr.r2@protonmail.com>
2023-06-15 21:05:36 -0400

    Add function to format orders data for proper display

--- frontend/components/OrderList.js
+++ frontend/components/OrderList.js
@@ -7,24 +7,67 @@ import tunnelURL from '../backend_tunnel';
 import { setOrders } from "../redux/actions";
 
 export default function OrderList() {
+
+  let formattedOrders = null;
+
+  function formatOrderData(orders) {
+
+    const ordersById = {};
+  
+    for (let i = 0; i < orders.length; i++) {
+      const order = orders[i];
+  
+      if (!ordersById[order.id]) {
+        // If this order id is not in ordersById, add it
+        ordersById[order.id] = {
+          id: order.id,
+          user_id: order.user_id,
+          first_name: order.first_name,
+          email: order.email,
+          total_price_cents: order.total_price_cents,
+          longitude: order.longitude,
+          latitude: order.latitude,
+          created_at: order.created_at,
+          line_items: [],
+        };
+      }
+  
+      // Add the line item to the appropriate order
+      ordersById[order.id].line_items.push({
+        name: order.name,
+        line_price_cents: order.line_price_cents,
+        quantity: order.quantity,
+      });
+    }
+  
+    // Convert the orders from an object to an array
+    return Object.values(ordersById);
+  }
+  
+
+
   //useDispatch hook dispatches Redux action
   const dispatch = useDispatch();
 
   //useSelector hook selects orders state from Redux
   const {orders} = useSelector((state) => state.reducer);
-  console.log("orders in OrderList: ", orders.orders);
+  // console.log("orders in OrderList: ", orders.orders);
 
   const fetchOrders = () => {
     
       //GET request to server to fetch orders data
-      axios.get(`${tunnelURL}/orders`)
-      // Dispatch setOrders action with orders data as payload
-      // to update the state in Redux
-      .then((res) => dispatch(setOrders(res.data))) 
+      axios.get(`${tunnelURL}/orders/user/1`)
+      //Format orders data and dispatch to Redux to set state
+      .then((res) => {
+        const formattedData = formatOrderData(res.data);
+        dispatch(setOrders(res.data))
+      })
       .catch((err) => {
         console.log(err);
       });
   };
+
+  
 
   //fetchOrders once component loaded
   useEffect(() => {
@@ -32,7 +75,7 @@ export default function OrderList() {
   }, []);
 
   const Order = ( {order} ) => {
-  console.log("order from within component: ", order);
+  // console.log("order from within component: ", order);
      return (
 
   <View>
