e617197527ca4e7b13095391ab24558343a41275
Viktor Ristic <vktr.r2@protonmail.com>
2023-06-15 23:22:55 -0400

    Display orders list data in component

--- frontend/components/OrderList.js
+++ frontend/components/OrderList.js
@@ -2,23 +2,22 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Text, View, Image, Button, FlatList } from "react-native";
 import axios from "axios";
-import tunnelURL from '../backend_tunnel';
+import tunnelURL from "../backend_tunnel";
+import styles from "../styles/ordersList";
 
 import { setOrders } from "../redux/actions";
 
 export default function OrderList() {
 
-  let formattedOrders = null;
-
-  function formatOrderData(orders) {
-
+  function formatOrderData(data) {
+    const ordersData = data.orders
     const ordersById = {};
-  
-    for (let i = 0; i < orders.length; i++) {
-      const order = orders[i];
-  
+
+    for (let i = 0; i < ordersData.length; i++) {
+      const order = ordersData[i];
+
       if (!ordersById[order.id]) {
-        // If this order id is not in ordersById, add it
+        // If this order id is not in ordersById, add new object for it
         ordersById[order.id] = {
           id: order.id,
           user_id: order.user_id,
@@ -31,7 +30,7 @@ export default function OrderList() {
           line_items: [],
         };
       }
-  
+
       // Add the line item to the appropriate order
       ordersById[order.id].line_items.push({
         name: order.name,
@@ -39,28 +38,21 @@ export default function OrderList() {
         quantity: order.quantity,
       });
     }
-  
-    // Convert the orders from an object to an array
+    
+    // Convert the data from an object to an array for FlatList to work
     return Object.values(ordersById);
   }
   
 
-
-  //useDispatch hook dispatches Redux action
-  const dispatch = useDispatch();
-
-  //useSelector hook selects orders state from Redux
-  const {orders} = useSelector((state) => state.reducer);
-  // console.log("orders in OrderList: ", orders.orders);
-
   const fetchOrders = () => {
-    
-      //GET request to server to fetch orders data
-      axios.get(`${tunnelURL}/orders/user/1`)
+    //GET request to server to fetch orders data
+    axios
+      .get(`${tunnelURL}/orders/user/1`)
       //Format orders data and dispatch to Redux to set state
       .then((res) => {
         const formattedData = formatOrderData(res.data);
-        dispatch(setOrders(res.data))
+        console.log("formattedData: ", formattedData);
+        dispatch(setOrders(formattedData));
       })
       .catch((err) => {
         console.log(err);
@@ -68,26 +60,48 @@ export default function OrderList() {
   };
 
   
-
   //fetchOrders once component loaded
   useEffect(() => {
     fetchOrders();
   }, []);
 
-  const Order = ( {order} ) => {
-  // console.log("order from within component: ", order);
-     return (
 
-  <View>
-      <Text>{`Order ID: ${order.id}`}</Text>
-    </View>
-  )};
+  //useDispatch hook dispatches Redux action
+  const dispatch = useDispatch();
+  
+  
+  //useSelector hook selects orders state from Redux
+  const { orders } = useSelector((state) => state.reducer);
+  // console.log("orders in OrderList: ", orders.orders);
+
+
+  const Order = ({order}) => {
+    console.log("Order WITHIN component: ", order);
+    return (
+      <View style={styles.list}>
+        <Text>{`Order ID: ${order.id}`}</Text>
+        {order.line_items.map((item, index) => (
+          <View key={index}>
+            <Text>{`Item: ${item.name}`}</Text>
+            <Text>{`Quantity: ${item.quantity}`}</Text>
+            <Text>{`Price: ${item.line_price_cents}`}</Text>
+          </View>
+        ))}
+        <Text>{`Latitude: ${order.latitude} | Longitude: ${order.longitude}`}</Text>
+        <Text>{`Date Ordered: ${order.created_at}`}</Text>
+        <Text>{`Total Price: ${order.total_price_cents}`}</Text>
+      </View>
+    );
+  };
 
   return (
+    <View>
+      {orders.length > 0 && <Text>{`${orders[0].first_name}'s Orders`}</Text>}
     <FlatList
-      data={orders.orders} //Pass orders data to FlatList
-      renderItem={(order) => <Order order={order.item} />}
-      keyExtractor={(order) => order.id}
-    />
+      data={orders} //Pass orders data to FlatList
+      renderItem={({item}) => <Order order={item} />}
+      keyExtractor={(order) => order.id.toString()}
+      />
+      </View>
   );
 }
--- server/db/queries/orders.js
+++ server/db/queries/orders.js
@@ -13,7 +13,7 @@ const getOrders = () => {
 const getOrdersByUserId = (id) => {
   return db
     .query(
-      "SELECT * FROM orders JOIN users on orders.user_id = users.id WHERE user_id = $1 ORDER BY orders.created_at DESC;",
+      "SELECT orders.id, orders.user_id, orders.total_price_cents, orders.longitude, orders.latitude, orders.created_at, line_items.quantity, line_items.line_price_cents, products.name, users.first_name, users.email FROM orders JOIN line_items ON orders.id = line_items.order_id JOIN products ON line_items.product_id = products.id JOIN users ON users.id = orders.user_id WHERE orders.user_id = $1 ORDER BY orders.created_at DESC;",
       [id]
     )
     .then((data) => {
