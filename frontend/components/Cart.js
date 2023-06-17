7b3c52974f6dac06c0bdab6cfb1c7bee4b7f214c
SirGraybon <graydonritchie@gmail.com>
2023-06-16 20:51:10 -0400

    improves cart styling

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -7,6 +7,7 @@ import {
   Modal,
   Pressable,
   BlurView,
+  ScrollView,
 } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "../styles/cart";
@@ -29,24 +30,48 @@ export default function Cart({ navigation }) {
 
   const Item = ({ item }) => (
     <View style={styles.item}>
-      <Text>{item.name}</Text>
-      <Text>{item.description}</Text>
-      <Button
-        onPress={() => dispatch(adjustCartQuantity(item, "+"))}
-        title="+"
-      />
-      <Text>{item.default_quantity}</Text>
-      <Button
-        onPress={() => dispatch(adjustCartQuantity(item, "-"))}
-        title="-"
-      />
-      <Button
-        onPress={() => dispatch(adjustCartQuantity(item, "delete"))}
-        title="X"
-      />
-      <Text>
-        ${((item.price_cents / 100) * item.default_quantity).toFixed(2)}
-      </Text>
+      <View style={styles.containerFlow}>
+        <View style={styles.containerFlow}>
+          <Image
+            style={styles.image}
+            source={require("../assets/Juniper_Twitter_Art.webp")}
+          />
+
+          <View style={styles.containerMid}>
+            <View>
+              <Text style={styles.title}>{item.name}</Text>
+              <Text style={styles.description}>{item.description}</Text>
+            </View>
+
+            <View style={styles.quantityControls}>
+              <Pressable
+                onPress={() => dispatch(adjustCartQuantity(item, "-"))}
+              >
+                <Text style={styles.quantityComponent}>-</Text>
+              </Pressable>
+
+              <Text style={styles.quantityComponent}>{item.default_quantity}</Text>
+
+              <Pressable
+                onPress={() => dispatch(adjustCartQuantity(item, "+"))}
+              >
+                <Text style={styles.quantityComponent}>+</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.containerEnd}>
+          <Pressable
+            onPress={() => dispatch(adjustCartQuantity(item, "delete"))}
+          >
+            <Text style={styles.quantityComponent}>⨉</Text>
+          </Pressable>
+          <Text style={styles.orderTotal}>
+            ${((item.price_cents / 100) * item.default_quantity).toFixed(2)}
+          </Text>
+        </View>
+      </View>
     </View>
   );
 
@@ -57,51 +82,51 @@ export default function Cart({ navigation }) {
   const total = beforeTax + tax;
 
   return (
-    
     <View style={styles.container}>
-      
-        <FlatList
-          style={styles.list}
-          data={cart}
-          renderItem={(item) => <Item item={item.item} />}
-          keyExtractor={(item) => item.id}
+      <ScrollView>
+
+      <FlatList
+        style={styles.list}
+        data={cart}
+        renderItem={(item) => <Item item={item.item} />}
+        keyExtractor={(item) => item.id}
         />
+        </ScrollView>
 
-        <View style={styles.total}>
-          <View style={styles.lineItem}>
-            <Text>Items ({cartNotification}):</Text>
-            <Text>${subTotal.toFixed(2)}</Text>
-          </View>
-
-          <View style={styles.lineItem}>
-            <Text>Delivery fee:</Text>
-            <Text>${deliveryFee.toFixed(2)}</Text>
-          </View>
-
-          <View style={styles.divider}>
-            <View style={styles.lineItem}>
-              <Text>Total before tax:</Text>
-              <Text>${beforeTax.toFixed(2)}</Text>
-            </View>
-
-            <View style={styles.lineItem}>
-              <Text>Estimated tax:</Text>
-              <Text>${tax.toFixed(2)}</Text>
-            </View>
-
-            <View style={styles.lineItem}>
-              <Text style={styles.orderTotal}>Order Total:</Text>
-              <Text style={styles.orderTotal}>${total.toFixed(2)}</Text>
-            </View>
-          </View>
-          <Pressable
-            style={styles.submitButton}
-            onPress={() => navigation.navigate("Map")}
-          >
-            <Text>Select Drop-off Location</Text>
-          </Pressable>
+      <View style={styles.total}>
+        <View style={styles.lineItem}>
+          <Text>Items ({cartNotification}):</Text>
+          <Text>${subTotal.toFixed(2)}</Text>
         </View>
-      
+
+        <View style={styles.lineItem}>
+          <Text>Delivery fee:</Text>
+          <Text>${deliveryFee.toFixed(2)}</Text>
+        </View>
+
+        <View style={styles.divider}>
+          <View style={styles.lineItem}>
+            <Text>Total before tax:</Text>
+            <Text>${beforeTax.toFixed(2)}</Text>
+          </View>
+
+          <View style={styles.lineItem}>
+            <Text>Estimated tax:</Text>
+            <Text>${tax.toFixed(2)}</Text>
+          </View>
+
+          <View style={styles.lineItem}>
+            <Text style={styles.orderTotal}>Order Total:</Text>
+            <Text style={styles.orderTotal}>${total.toFixed(2)}</Text>
+          </View>
+        </View>
+        <Pressable
+          style={styles.submitButton}
+          onPress={() => navigation.navigate("Map")}
+        >
+          <Text>Select Drop-off Location</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
--- frontend/styles/cart.js
+++ frontend/styles/cart.js
@@ -2,181 +2,236 @@ import { StyleSheet } from "react-native";
 import { Platform } from "react-native";
 
 const device = Platform.OS;
-let styles
+let styles;
 
+if (device === "web") {
+  styles = StyleSheet.create({
+    container: {
+      display: "flex",
+      flexDirection: "row",
+      width: "80%",
+      alignSelf: "center",
+      marginTop: 10,
+    },
+    image: {
+      height: 100,
+      width: 100,
+      margin: 5,
+    },
 
+    box: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "#F5FCFF",
+      borderRadius: 15,
+    },
 
+    item: {
+      backgroundColor: "white",
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+      borderRadius: 10,
+      shadowColor: "grey",
+      shadowOffset: { width: 3, height: 3 },
+      shadowRadius: 10,
+      minWidth: 530
+    },
+    total: {
+      backgroundColor: "white",
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+      borderRadius: 10,
+      shadowColor: "grey",
+      shadowOffset: { width: 3, height: 3 },
+      shadowRadius: 10,
+      width: 350,
+      height: 250,
+    },
 
-if (device === "web"){
+    submitButton: {
+      borderWidth: 1,
+      padding: 10,
+      margin: 10,
+      borderRadius: 5,
+      backgroundColor: "#55bb55",
+      width: "auto",
+      textAlign: "center",
+    },
 
+    lineItem: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginLeft: 15,
+      marginRight: 15,
+    },
+    divider: {
+      borderTopWidth: 1,
+      borderColor: "lightgrey",
+      marginTop: 5,
+      paddingTop: 5,
+    },
+    orderTotal: {
+      fontSize: 20,
+      fontWeight: "bold",
+      color: "#ce4216",
+      marginTop: 5,
+    },
+    quantityControls: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "flex-end",
+      borderBottomWidth: 1,
+      borderBottomColor: "lightgrey"
+    },
+    containerFlow: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      flexShrink: 1,
+    },
+    containerEnd: {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "flex-end",
+    },
+    containerMid: {
+      display: "flex",
+      justifyContent: "space-between",
+    },
 
-styles = StyleSheet.create({
-
-
-
-  container: {
-    display: "flex",
-    flexDirection: "row",
-    width: "80%",
-    alignSelf: "center",
-    marginTop: 10,
-  },
-
-  container: {
-    display: "flex",
-    flexDirection: "row",
-    width: "80%",
-    alignSelf: "center",
-    marginTop: 10,
-  },
-
-  box: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#F5FCFF",
-    borderRadius: 15,
-  },
-
-  item: {
-    backgroundColor: "white",
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    borderRadius: 10,
-    shadowColor: "grey",
-    shadowOffset: { width: 3, height: 3 },
-    shadowRadius: 10,
-  },
-  total: {
-    backgroundColor: "white",
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    borderRadius: 10,
-    shadowColor: "grey",
-    shadowOffset: { width: 3, height: 3 },
-    shadowRadius: 10,
-    width: "auto",
-    height: 250,
-  },
-
-  submitButton: {
-    borderWidth: 1,
-    padding: 10,
-    margin: 10,
-    borderRadius: 5,
-    backgroundColor: "#55bb55",
-    width: "auto",
-    textAlign: "center",
-  },
-
-  lineItem: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginLeft: 15,
-    marginRight: 15,
-  },
-  divider: {
-    borderTopWidth: 1,
-    borderColor: "lightgrey",
-    marginTop: 5,
-    paddingTop: 5,
-  },
-  orderTotal: {
-    fontSize: 25,
-    fontWeight: "bold",
-    color: "#ce4216",
-    marginTop: 5,
-  },
-  list: {
-  },
-});
-
-
+    list: {},
+    title: {
+      fontSize: 20,
+      fontWeight: "bold",
+    },
+    description: {
+      marginTop: 5,
+      display: "flex",
+      width: 300,
+    },
+    quantityComponent: {
+      fontSize: 20,
+      fontWeight: "bold",
+      marginLeft: 10,
+    },
+  });
 } else {
-  
+  styles = StyleSheet.create({
+    container: {
+      display: "flex",
+      width: "90%",
+      alignSelf: "center",
+      marginTop: 10,
+    },
+    image: {
+      height: 100,
+      width: 100,
+      margin: 5,
+    },
 
-styles = StyleSheet.create({
+    box: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "#F5FCFF",
+      borderRadius: 15,
+    },
 
+    item: {
+      backgroundColor: "white",
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+      borderRadius: 10,
+      shadowColor: "grey",
+      shadowOffset: { width: 3, height: 3 },
+      shadowRadius: 10,
+    },
+    total: {
+      backgroundColor: "white",
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+      borderRadius: 10,
+      shadowColor: "grey",
+      shadowOffset: { width: 3, height: 3 },
+      shadowRadius: 10,
+      width: 350,
+      height: 250,
+    },
 
+    submitButton: {
+      borderWidth: 1,
+      padding: 10,
+      margin: 10,
+      borderRadius: 5,
+      backgroundColor: "#55bb55",
+      width: "auto",
+      textAlign: "center",
+    },
 
+    lineItem: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginLeft: 15,
+      marginRight: 15,
+    },
+    divider: {
+      borderTopWidth: 1,
+      borderColor: "lightgrey",
+      marginTop: 5,
+      paddingTop: 5,
+    },
+    orderTotal: {
+      fontSize: 20,
+      fontWeight: "bold",
+      color: "#ce4216",
+      marginTop: 5,
+    },
+    quantityControls: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "flex-end",
+      borderBottomWidth: 1,
+      borderBottomColor: "lightgrey"
+    },
+    containerFlow: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      flexShrink: 1,
+    },
+    containerEnd: {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "flex-end",
+    },
+    containerMid: {
+      display: "flex",
+      justifyContent: "space-between",
+    },
 
-  container: {
-    display: "flex",
-    width: "80%",
-    alignSelf: "center",
-    marginTop: 10,
-  },
-
-
-
-  box: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#F5FCFF",
-    borderRadius: 15,
-  },
-
-  item: {
-    backgroundColor: "white",
-    padding: 10,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    borderRadius: 10,
-    shadowColor: "grey",
-    shadowOffset: { width: 3, height: 3 },
-    shadowRadius: 10,
-  },
-  total: {
-    backgroundColor: "white",
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    borderRadius: 10,
-    shadowColor: "grey",
-    shadowOffset: { width: 3, height: 3 },
-    shadowRadius: 10,
-    width: "auto",
-    height: 250,
-  },
-
-  submitButton: {
-    borderWidth: 1,
-    padding: 10,
-    margin: 10,
-    borderRadius: 5,
-    backgroundColor: "#55bb55",
-    width: "auto",
-    textAlign: "center",
-  },
-
-  lineItem: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginLeft: 15,
-    marginRight: 15,
-  },
-  divider: {
-    borderTopWidth: 1,
-    borderColor: "lightgrey",
-    marginTop: 5,
-    paddingTop: 5,
-  },
-  orderTotal: {
-    fontSize: 25,
-    fontWeight: "bold",
-    color: "#ce4216",
-    marginTop: 5,
-  },
-  list: {
-  },
-});
-
+    list: {},
+    title: {
+      fontSize: 20,
+      fontWeight: "bold",
+    },
+    description: {
+      marginTop: 5,
+      display: "flex",
+      width: 300,
+    },
+    quantityComponent: {
+      fontSize: 20,
+      fontWeight: "bold",
+      marginLeft: 10,
+    },
+  });
 }
 
 export default styles;
-
