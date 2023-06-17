6581af04941b3d64d2c0638bab904e18b5852e49
Viktor Ristic <vktr.r2@protonmail.com>
2023-06-17 17:38:45 -0400

    Add confirmation component for mobile and web

---
+++ frontend/components/ConfirmationMobile.js
@@ -0,0 +1,98 @@
+import { useState, useCallback, useEffect } from "react";
+import { Text, View, TextInput, Pressable, ActivityIndicator } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import MapView, { Marker } from "react-native-maps";
+import key from "../api_key";
+import * as Location from "expo-location";
+import styles from '../styles/map'
+import { setLocationInfo } from "../redux/actions";
+
+export default function Map({navigation}) {
+
+  const dispatch = useDispatch();
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [markerPosition, setMarkerPosition] = useState(null);
+  const [locationDetails, setLocationDetails] = useState(''); 
+
+  // grab device location using expo-location
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      // if location permission not granted by user, return error
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      // Capture current location
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setMarkerPosition(location.coords);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  // save either error message or JSON location data (in string format) in 'text' variable
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  //Set new markerPosition state onPress of map
+  const handlePress = (event) => {
+    setMarkerPosition(event.nativeEvent.coordinate);
+  };
+
+  const checkoutConfirmation = function (markerPosition, locationDetails) {
+    const input = { markerPosition, locationDetails };
+    dispatch(setLocationInfo(input))
+        navigation.navigate("Stripe");
+  };
+
+  return (
+    // short circuit triggers re-render of component once location truthy
+
+    location ? (
+      <View style={styles.container}>
+      <Text style={styles.title}>Set Location</Text>
+      <Text style={styles.subtitle}>Let Us Know Exactly Where You'll Be</Text>
+        <MapView
+          style={styles.map}
+          provider="google"
+          googleMapsApiKey={key}
+          loadingFallback={<Text>Loading...</Text>}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0115,
+            longitudeDelta: 0.0055,
+          }}
+          onPress={handlePress}
+        >
+          <Marker coordinate={markerPosition} />
+        </MapView>
+        <Text style={styles.infoText}>{`Please provide us with some more details so we can find you`}</Text>
+        <TextInput
+          style={styles.locationDetailsInput}
+          placeholder="Location Details"
+          editable
+          multiline
+          onChangeText={(text) => setLocationDetails(text)}
+          value={locationDetails}
+          numberOfLines={5}
+          maxLength={255}
+        />
+        <Pressable
+          style={styles.checkoutButton}
+          onPress={() => checkoutConfirmation(markerPosition, locationDetails)}
+        >
+          <Text style={styles.buttonText}>Proceed to Checkout</Text>
+        </Pressable>
+      </View>
+    ) : <ActivityIndicator size="large" color="#00ff00" style={styles.activityIndicator} /> 
+  );
+}
---
+++ frontend/components/ConfirmationWeb.js
@@ -0,0 +1,116 @@
+import React, { useState, useCallback, useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Text, TextInput, Pressable, View, ActivityIndicator } from "react-native";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import key from "../api_key";
+import * as Location from "expo-location";
+import styles from "../styles/webMap";
+import { setLocationInfo } from "../redux/actions";
+
+export default function WebMap({ navigation }) {
+  const { locationInfo } = useSelector((state) => state.reducer);
+  const dispatch = useDispatch();
+
+  const [location, setLocation] = useState({
+    coords: { latitude: 0, longitude: 0 },
+  });
+  const [markerPos, setMarkerPos] = useState(location);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const [locationDetails, setLocationDetails] = useState("");
+
+  const { isLoaded, loadError } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: key,
+  });
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setMarkerPos(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  // save either error message or JSON location data (in string format) in 'text' variable
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  const updateMarker = (ev) => {
+    const oldMarkerPos = { ...markerPos };
+    setMarkerPos(
+      oldMarkerPos,
+      (oldMarkerPos.coords.latitude = ev.latLng.lat()),
+      (oldMarkerPos.coords.longitude = ev.latLng.lng())
+    );
+  };
+
+  const checkoutConfirmation = function (markerPos, locationDetails) {
+    const input = { markerPos, locationDetails };
+    dispatch(setLocationInfo(input));
+    navigation.navigate("Stripe");
+  };
+
+  if (loadError) {
+    return <Text>Map cannot be loaded</Text>;
+  }
+
+  return  (
+    isLoaded ? (
+    <>
+      <Text style={styles.title}>Set Location</Text>
+      <Text style={styles.subtitle}>Let Us Know Exactly Where You'll Be</Text>
+      <View style={styles.container}>
+        <GoogleMap
+          mapContainerStyle={styles.mapWindow}
+          center={{
+            lat: location.coords.latitude,
+            lng: location.coords.longitude,
+          }}
+          zoom={13}
+          onClick={(ev) => {
+            updateMarker(ev);
+          }}
+        >
+          <Marker
+            position={{
+              lat: markerPos.coords.latitude,
+              lng: markerPos.coords.longitude,
+            }}
+            draggable
+            onDragEnd={(ev) => {
+              updateMarker(ev);
+            }}
+          />
+        </GoogleMap>
+        <Text style={styles.infoText}>{`Please provide us with some more details so we can find you`}</Text>
+        <TextInput
+          style={styles.locationDetailsInput}
+          placeholder="Location Details"
+          editable
+          multiline
+          onChangeText={(text) => setLocationDetails(text)}
+          value={locationDetails}
+          numberOfLines={5}
+          maxLength={255}
+        />
+        <Pressable
+          style={styles.checkoutButton}
+          onPress={() => checkoutConfirmation(markerPos, locationDetails)}
+        >
+          <Text style={styles.buttonText}>Proceed to Checkout</Text>
+        </Pressable>
+      </View>
+    </>
+  ) : <ActivityIndicator size="large" color="#00ff00" style={styles.activityIndicator} /> )
+}
