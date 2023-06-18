870ecefce2dc98bf144bfdc557bec8a9e1d8923b
MorriganH <morriganhennessy@gmail.com>
2023-06-18 16:54:49 -0400

    adds query to get potatoes

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -11,7 +11,7 @@ import {
   ScrollView,
   ImageBackground,
 } from "react-native";
-import Footer from "./Footer"
+import Footer from "./Footer";
 import { setUserSession } from "../redux/actions";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
@@ -30,7 +30,7 @@ export default function Home({ navigation }) {
   const [menuModalShow, setMenuModalShow] = useState(false);
 
   const { cart, products, modalShow, modalProduct, userSession } = useSelector(
-    (state) => state.reducer
+    state => state.reducer
   );
 
   const [orderCount, setOrderCount] = useState(0);
@@ -40,22 +40,32 @@ export default function Home({ navigation }) {
   const filter = function (path, view) {
     axios
       .get(`${tunnelURL}/products/${path}`)
-      .then((prods) => {
+      .then(prods => {
         dispatch(setProducts(prods.data.products));
       })
       .then(viewSwitcher(view))
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
+  };
+
+  const getPotato = function () {
+    axios
+      .get(`${tunnelURL}/products/potato`)
+      .then(prods => {
+        dispatch(setProducts(prods.data.products));
+      })
+      .then(viewSwitcher("ProductList"))
+      .catch(err => console.log(err));
   };
 
   const getOrderCount = function (user_id) {
     axios
       .get(`${tunnelURL}/orders/count/${user_id}`)
-      .then((res) => {
+      .then(res => {
         setOrderCount(res.data);
       })
-      .catch((err) => console.log(err));
+      .catch(err => console.log(err));
   };
 
   useEffect(() => {
@@ -103,7 +113,10 @@ export default function Home({ navigation }) {
           <Text style={styles.buttonTitle}>Family Packages</Text>
 
           <View style={styles.iconsHolder}>
-            <Pressable onPress={() => filter("addons", "ProductList")} style={styles.iconsGroup}>
+            <Pressable
+              onPress={() => filter("addons", "ProductList")}
+              style={styles.iconsGroup}
+            >
               <ImageBackground
                 style={styles.icons}
                 source={require("../assets/home_page_test/pexels-ron-lach-10398349.jpg")}
@@ -119,7 +132,7 @@ export default function Home({ navigation }) {
               <Text style={styles.iconsLabel}>Drinks</Text>
             </Pressable>
 
-            <Pressable style={styles.iconsGroup}>
+            <Pressable onPress={() => getPotato()} style={styles.iconsGroup}>
               <ImageBackground
                 style={styles.icons}
                 source={require("../assets/home_page_test/potato.jpg")}
@@ -138,10 +151,8 @@ export default function Home({ navigation }) {
             ></ImageBackground>
           </Pressable>
           <Text style={styles.buttonTitle}>Baskets for two</Text>
-
-          </View>
-          <View style={styles.webColumn}>
-
+        </View>
+        <View style={styles.webColumn}>
           <Pressable
             style={styles.button}
             onPress={() => filter("party", "ProductList")}
@@ -152,7 +163,7 @@ export default function Home({ navigation }) {
             ></ImageBackground>
           </Pressable>
           <Text style={styles.buttonTitle}>Party packages</Text>
-          
+
           <Pressable
             style={styles.button}
             onPress={() => filter("2", "ProductList")}
@@ -186,7 +197,7 @@ export default function Home({ navigation }) {
           </Pressable>
           <Text style={styles.buttonTitle}>Fun in the sun</Text>
 
-{/* 
+          {/* 
           <View style={styles.footer}>
             <Pressable style={styles.footerOptions}>
               <Text>All baskets</Text>
@@ -206,10 +217,10 @@ export default function Home({ navigation }) {
           <Text style={styles.buttonTitle}> TeamTBD™</Text> */}
         </View>
 
-        {device !== "web" && <Footer/> }
+        {device !== "web" && <Footer />}
       </ScrollView>
-        {device === "web" && <Footer/> }
-    
+      {device === "web" && <Footer />}
+
       <Modal
         visible={modalShow === "homeModal"}
         transparent={true}
--- server/db/queries/products.js
+++ server/db/queries/products.js
@@ -6,35 +6,64 @@ const getAllProducts = () => {
   });
 };
 
-const getProductById = (id) => {
+const getProductById = id => {
   return db.query("SELECT * FROM products WHERE id = $1;", [id]).then(data => {
     return data.rows;
   });
 };
 
-const getProductsByPortions = (portions) => {
-  return db.query("SELECT * FROM products WHERE portions = $1 AND is_basket = true;", [portions]).then(data => {
-    return data.rows;
-  });
+const getProductsByPortions = portions => {
+  return db
+    .query("SELECT * FROM products WHERE portions = $1 AND is_basket = true;", [
+      portions,
+    ])
+    .then(data => {
+      return data.rows;
+    });
 };
 
 const getPartyProducts = () => {
-  return db.query("SELECT * FROM products WHERE portions > 4 AND is_basket = true;").then(data => {
-    return data.rows;
-  });
+  return db
+    .query("SELECT * FROM products WHERE portions > 4 AND is_basket = true;")
+    .then(data => {
+      return data.rows;
+    });
 };
 
 const getDeluxeProducts = () => {
-  return db.query("SELECT * FROM products WHERE is_deluxe = TRUE;").then(data => 
-    {
+  return db
+    .query("SELECT * FROM products WHERE is_deluxe = TRUE;")
+    .then(data => {
       return data.rows;
-    })
+    });
 };
 const getIndividualProducts = () => {
-  return db.query("SELECT * FROM products WHERE is_basket = false;").then(data => 
-    {
+  return db
+    .query("SELECT * FROM products WHERE is_basket = false;")
+    .then(data => {
       return data.rows;
-    })
+    });
 };
 
-module.exports = { getAllProducts, getProductById, getProductsByPortions, getDeluxeProducts, getIndividualProducts, getPartyProducts };
+const getPotatoes = () => {
+  return db
+    .query(
+      `SELECT *
+    FROM products
+    WHERE name LIKE 'Potato%'
+    ;`
+    )
+    .then(data => {
+      return data.rows;
+    });
+};
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+  getProductsByPortions,
+  getDeluxeProducts,
+  getIndividualProducts,
+  getPartyProducts,
+  getPotatoes,
+};
